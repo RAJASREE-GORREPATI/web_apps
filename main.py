0c90a100436ae0d5f143f68cda616c119622f14c
rajasreegorrepati24@gmail.com <rajasreegorrepati24@gmail.com>
2023-09-15 12:47:11 +0530

    create column,image and description

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+col1,col2=st.columns(2)
+with col1:
+    st.image("images/photo.png")
+with col2:
+    st.title("Rajasree Gorrepati")
+    content="""Hi,I'm Rajasree Gorrepati.Iam a python programmer.I graduated in 2023 from R.V.R & J.C College of Engineering in the stream of computer science."""
+    st.info(content)
