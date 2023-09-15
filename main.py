d3d247a36e027985ca5422e64c98ed40e1be2dfe
rajasreegorrepati24@gmail.com <rajasreegorrepati24@gmail.com>
2023-09-15 12:57:17 +0530

    Add description

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ with col2:
     st.title("Rajasree Gorrepati")
     content="""Hi,I'm Rajasree Gorrepati.Iam a python programmer.I graduated in 2023 from R.V.R & J.C College of Engineering in the stream of computer science."""
     st.info(content)
+st.write("Below you can find some of the apps I have built in Python.Feel free to contact me!")
