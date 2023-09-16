8e2708e127c8f0b6679659fb1a8b25f63fdbe98b
rajasreegorrepati24@gmail.com <rajasreegorrepati24@gmail.com>
2023-09-16 12:00:16 +0530

    Add images,description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
 col1,col2=st.columns(2)
 with col1:
@@ -8,3 +9,17 @@ with col2:
     content="""Hi,I'm Rajasree Gorrepati.Iam a python programmer.I graduated in 2023 from R.V.R & J.C College of Engineering in the stream of computer science."""
     st.info(content)
 st.write("Below you can find some of the apps I have built in Python.Feel free to contact me!")
+col3,empty_col,col4=st.columns([1.5,0.5,1.5])
+df=pandas.read_csv("data.csv",sep=";")
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
