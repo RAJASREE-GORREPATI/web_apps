aaa81c563083e67c9e0a5562dbdc794702f5be5e
rajasreegorrepati24@gmail.com <rajasreegorrepati24@gmail.com>
2023-09-18 11:56:04 +0530

    send email from contact page

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,2 +1,16 @@
 import streamlit as st
+from send_email import send_email
 st.header("Contact Me")
+with st.form(key="email-form"):
+    user_email=st.text_input("Your email address")
+    message=st.text_area("Your message")
+    message=f"""\
+Subject:New email from {user_email} 
+
+From: {user_email}
+{message}
+"""
+    button=st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Your email has been sent successfully.")
